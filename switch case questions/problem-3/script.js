faf393d9292ce34fe2778283eb7145635e892231
mohd-Jainuddin <zainuddinshaikh3032@gmail.com>
2022-06-17 07:13:17 +0530

    Exercise-4

---
+++ switch case questions/problem-3/script.js
@@ -0,0 +1,9 @@
+let a=11,b=13
+let x=a>b?1:0
+switch(x){
+    case 0:
+        console.log(b+" is greater")
+        break
+    case 1:
+        console.log(a+" is greater")    
+}
---
+++ switch case questions/problem-4/script.js
@@ -0,0 +1,9 @@
+let a=3
+let b=a%2==0?1:0
+switch(b){
+    case 0:
+        console.log("ODD")
+        break
+    case 1:
+        console.log("EVEN")    
+}
